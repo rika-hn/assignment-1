7b8f307504ac3ea9419ceee66562a66a0b868970
rika-hanai <r-hanai@ntsco.jp>
2025-07-03 22:54:16 +0900

    一旦コミット

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
-import { React } from "react";
+import React from "react";
+
+import { useState } from "react";
 
 export const App = () => {
-  const records = [
-    { title: "勉強の記録1", time: 1 },
-    { title: "勉強の記録2", time: 3 },
-    { title: "勉強の記録3", time: 5 },
-  ];
+  const [studyContent, setStudyContent] = useState("");
+  const [studyTotalTime, setStudyTotalTime] = useState("");
+
+  const [records, setRecords] = useState([]);
+
+  const onChangeStudyContent = (event) => {
+    setStudyContent(event.target.value);
+  };
+
+  const onChangeStudyTotalTime = (event) => {
+    setStudyTotalTime(event.target.value);
+  };
+
+  const onClickRegister = () => {
+    if (studyContent === "") return;
+    const newStudyContent = [...records, studyContent];
+    const newStudyTotalTime = [...records, studyTotalTime];
+    setRecords([newStudyContent], [newStudyTotalTime]);
+    setStudyContent("");
+    setStudyTotalTime("");
+  };
 
   return (
     <>
       <h1>学習記録一覧</h1>
       <div>
+        学習内容
+        <input
+          type="text"
+          value={studyContent}
+          onChange={onChangeStudyContent}
+        />
+      </div>
+      <div>
+        学習時間
+        <input
+          type="number"
+          value={studyTotalTime}
+          onChange={onChangeStudyTotalTime}
+        />
+        時間
+      </div>
+      <div>入力されている学習内容: {studyContent}</div>
+      <div>入力されている時間: {studyTotalTime}</div>
+      <div>
         {records.map((record, key) => (
           <div key={key} style={{ display: "flex" }}>
-            <p style={{ paddingRight: "4px" }}>{records[key].title}</p>
-            <p>{records[key].time}時間</p>
+            <p>{records}</p>
           </div>
         ))}
       </div>
+      <button onClick={onClickRegister}>登録</button>
     </>
   );
 };
