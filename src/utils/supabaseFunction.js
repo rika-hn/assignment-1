7fad316583dfd40033497579cb41a6c8cbee6b06
rika-hanai <r-hanai@ntsco.jp>
2025-08-01 12:01:28 +0900

    feat: ローディング機能を追加

--- src/utils/supabaseFunction.js
+++ src/utils/supabaseFunction.js
@@ -1,6 +1,6 @@
 import { supabase } from "../utils/supabase";
 
 export const getAllRecords = async () => {
-  const records = await supabase.from("records").select("*");
+  const records = await supabase.from("study-record").select("*");
   return records.data;
 };
