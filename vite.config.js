f90dd81a4512f32cb34e9fab21484700f3618e64
rika-hanai <r-hanai@ntsco.jp>
2025-08-22 00:04:22 +0900

    test commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import env from "vite-plugin-env-compatible";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [
